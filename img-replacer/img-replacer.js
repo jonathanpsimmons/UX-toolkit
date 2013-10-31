2757ea6c13a3e1747cb32c945396da5dac95a410
Patrick Lewis <plewis@homeaway.com>
2013-10-31 14:31:50 -0500

    converted to plugin with options

--- img-replacer/img-replacer.js
+++ img-replacer/img-replacer.js
@@ -11,153 +11,198 @@ Author: Jonathan P Simmons
 documentation URL:http://mockups/HASP/Product/js/Code-Documentation.html
 
 */
+;(function($, document, window, undefined) {
+    // Optional, but considered best practice by some
+    "use strict";
+
+    // Name the plugin so it's only in one place
+    var pluginName = 'UXimgReplacer';
+
+    // Default options for the plugin as a simple object
+    var defaults = {
+        mouseover: "color"  // "color" or "img url"
+    };
+
+    // Plugin constructor
+    // This is the boilerplate to set up the plugin to keep our actual logic in one place
+    function Plugin(element, options) {
+        this.element = element;
+        // Merge the options given by the user with the defaults
+        this.options = $.extend({}, defaults, options)
+
+        // Attach data to the elment
+        this.$el      = $(element);
+        this.$el.data(name, this);
+
+        this._defaults = defaults;
+
+        var meta      = this.$el.data(name + '-opts');
+        this.opts     = $.extend(this._defaults, options, meta);
+
+        console.log(this.opts.imgReplacer);
+
+        // Initialization code to get the ball rolling
+        // If your plugin is simple, this may not be necessary and
+        // you could place your implementation here
+
+        //counting variables
+        var i;
+        var j = 0;
+        var multiple = 0;
+        var that   = this;
 
 
+       $('img').each(function() {
 
-$(document).ready(function() {
+                var theImg = $(this);
+                var imgPos = theImg.position();
 
-   //counting variables
-   var i;
-   var j = 0;
-   var multiple = 0;
+                // if usemap exists, set variable to attr usemap
+                if ( $(this).attr('usemap') ) {
 
-   $('img').each(function() {
+                    //where to add 'no-hover' check? Here?
 
-            var theImg = $(this);
-            var imgPos = theImg.position();
+                    //delete '#' from usemap attr
+                    var useMapName = $(this).attr('usemap');
 
-            // if usemap exists, set variable to attr usemap
-            if ( $(this).attr('usemap') ) {
-
-                //where to add 'no-hover' check? Here?
-
-                //delete '#' from usemap attr
-                var useMapName = $(this).attr('usemap');
-
-                if (useMapName.charAt('0') === '#') {
-                    useMapName = useMapName.substr(1);
-                }
+                    if (useMapName.charAt('0') === '#') {
+                        useMapName = useMapName.substr(1);
+                    }
 
 
-                //iterate through all of the MAP tags,
-                $('map').each( function() {
+                    //iterate through all of the MAP tags,
+                    $('map').each( function() {
 
-                    if ( $(this).attr('name') == useMapName ) {
+                        if ( $(this).attr('name') == useMapName ) {
 
-                        //iterating through each <area> tag within an image map
-                        $('area', this).not('.no-hover').each(function(){
+                            //iterating through each <area> tag within an image map
+                            $('area', this).not('.no-hover').each(function(){
 
-                            if ( i == undefined ) {
-                                i=0;
-                            } else {
-                                i++;
-                            }
+                                if ( i == undefined ) {
+                                    i=0;
+                                } else {
+                                    i++;
+                                }
 
-                            //this is a magic number, not sure where it's coming from
-                            if ( $('area').length > 1 ) {
-                                multiple = 5;
-                            }
+                                //this is a magic number, not sure where it's coming from
+                                if ( $('area').length > 1 ) {
+                                    multiple = 5;
+                                }
 
-                            // returns coordinates of each <AREA> in an array
-                            //the values in the array will be used to construct a DIV on top of each <AREA> that will be hoverable
-                            var coord_vals = $(this).attr('coords').split(',');
-                            var divHeight = (parseInt(coord_vals[2]) - parseInt(coord_vals[0]));
-                            var divWidth = (parseInt(coord_vals[3]) - parseInt(coord_vals[1]));
-                            var divTop = parseInt(coord_vals[1]) + imgReplacer[2];
-                            var divLeft = parseInt(coord_vals[0]);
-                            var bckgrnd;
+                                // returns coordinates of each <AREA> in an array
+                                //the values in the array will be used to construct a DIV on top of each <AREA> that will be hoverable
+                                var coord_vals = $(this).attr('coords').split(',');
+                                var divHeight = (parseInt(coord_vals[2]) - parseInt(coord_vals[0]));
+                                var divWidth = (parseInt(coord_vals[3]) - parseInt(coord_vals[1]));
+                                var divTop = parseInt(coord_vals[1]) + that.opts.imgReplacer[2];
+                                var divLeft = parseInt(coord_vals[0]);
+                                var bckgrnd;
+                                var opcty;
 
-                            //allowing the mouseover to just be a color
-                            if(imgReplacer[0] == 'color' && imgReplacer[4] != 'undefined') {
+                                //allowing the mouseover to just be a color
+                                if(that.opts.mouseover == 'color' && that.opts.imgReplacer[4] != 'undefined') {
 
-                                bckgrnd = '#' + imgReplacer[4];
-                                opcty = '.3';
+                                    bckgrnd = '#' + that.opts.imgReplacer[4];
+                                    opcty = '.3';
 
-                            } else if(imgReplacer[0] != 'color') {
+                                } else if(that.opts.mouseover != 'color') {
 
-                                bckgrnd = 'url(' + imgReplacer[0] + ') no-repeat';  //imgReplacer[0] from the passed variable
-                                opcty = '1';
+                                    bckgrnd = 'url(' + that.opts.mouseover + ') no-repeat';  // mouseover from the passed variable
+                                    opcty = '1';
 
-                            } else {
+                                } else {
 
-                                console.log("There's a problem with one of the variables being passed. Are you trying to pass a color or image?");
+                                    console.log("There's a problem with one of the variables being passed. Are you trying to pass a color or image?");
 
-                            }
-
-
-                            //Append the DIV to the body, give it CSS attributes
-                            //v in classname is used elsewhere in function
-                            $('<div class="map-div' + ' v' + i + '"></div>').appendTo('body').css({
-
-                                'background': bckgrnd,
-                                'background-position':'right center',
-                                'display': 'none',  //this seems to be the problem
-                                'z-index':'5000',
-                                'opacity': opcty,
-                                'width':divHeight,                         //from above
-                                'height':divWidth,                         //from above
-                                'position':'absolute',
-                                'top':(parseInt(divTop + imgPos.top)), //from above
-                                'left':'50%',
-                                'margin-left': (divLeft - imgReplacer[1])  //from above
-
-                            });
+                                }
 
 
-                            //connect the <AREA> with the <DIV> above it using the same class…?
-                            $(this).addClass(' v' + i);
+                                //Append the DIV to the body, give it CSS attributes
+                                //v in classname is used elsewhere in function
+                                $('<div class="map-div' + ' v' + i + '"></div>').appendTo('body').css({
 
-                            var classname = $(this).attr('class');
-                            var vClass = classname.substr(classname.indexOf('v') + 1 ); //returns only the numeric value
+                                    'background': bckgrnd,
+                                    'background-position':'right center',
+                                    'display': 'none',  //this seems to be the problem
+                                    'z-index':'5000',
+                                    'opacity': opcty,
+                                    'width':divHeight,                         //from above
+                                    'height':divWidth,                         //from above
+                                    'position':'absolute',
+                                    'top':(parseInt(divTop + imgPos.top)), //from above
+                                    'left':'50%',
+                                    'margin-left': (divLeft - that.opts.imgReplacer[1])  //from above
+
+                                });
 
 
-                            //make sure that if there's white space that I take it away
-                            if (vClass.indexOf(' ') != -1) {
-                                vClass = vClass.trim();
-                            }
+                                //connect the <AREA> with the <DIV> above it using the same class…?
+                                $(this).addClass(' v' + i);
 
-                            // I've bound a hover to the <AREA> tag, now I need co-ordinate bringing up the correct <DIV>
-                            $(this).bind('mouseenter', function () {
+                                var classname = $(this).attr('class');
+                                var vClass = classname.substr(classname.indexOf('v') + 1 ); //returns only the numeric value
 
-                                    $('.map-div' + '.v' + vClass).css('display','block').hover(
 
-                                        function () {                           //mouse enter
+                                //make sure that if there's white space that I take it away
+                                if (vClass.indexOf(' ') != -1) {
+                                    vClass = vClass.trim();
+                                }
 
-                                            $('.map-div').css('display','none');
-                                            $(this).css({
-                                                'display':'block',
-                                                'cursor': 'pointer'
-                                            });
+                                // I've bound a hover to the <AREA> tag, now I need co-ordinate bringing up the correct <DIV>
+                                $(this).bind('mouseenter', function () {
 
-                                    },function () {                             //mouse leave
+                                        $('.map-div' + '.v' + vClass).css('display','block').hover(
 
-                                            $('.map-div').css('display','none');
-                                            $(this).css({
-                                                'display':'none',
-                                                'cursor': 'pointer'
-                                            });
+                                            function () {                           //mouse enter
 
-                                    }).click ( function () {
+                                                $('.map-div').css('display','none');
+                                                $(this).css({
+                                                    'display':'block',
+                                                    'cursor': 'pointer'
+                                                });
 
-                                        if (imgReplacer[3] == true && imgReplacer[3] != '#') { //make sure there's been a URL passed and it's not #
+                                        },function () {                             //mouse leave
 
-                                            window.location = imgReplacer[3];
+                                                $('.map-div').css('display','none');
+                                                $(this).css({
+                                                    'display':'none',
+                                                    'cursor': 'pointer'
+                                                });
 
-                                        }
-                                    });
+                                        }).click ( function () {
 
-                            });
+                                            if (that.opts.imgReplacer[3] == true && that.opts.imgReplacer[3] != '#') { //make sure there's been a URL passed and it's not #
 
-                        });     //end area each
+                                                window.location = that.opts.imgReplacer[3];
 
-                    }  //end if
+                                            }
+                                        });
 
-                }); // end each img
+                                });
 
-            }  //end if
+                            });     //end area each
 
-            j++;
+                        }  //end if
 
-   }); //end img.each()
+                    }); // end each img
 
-}); //end document read
+                }  //end if
+
+                j++;
+
+       }); //end img.each()
+
+    }
+
+    $.fn[pluginName] = function(options) {
+        // Iterate through each DOM element and return it
+        return this.each(function() {
+            // prevent multiple instantiations
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+            }
+        });
+    };
+
+
+})(jQuery, document, window);
